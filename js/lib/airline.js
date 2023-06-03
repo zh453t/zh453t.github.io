86f9436808d75baf088b56a9004724ca463de18a
zh453t <zhangtinglu@live.cn>
2023-06-03 21:18:50 +0800

    June1

---
+++ js/lib/airline.js
@@ -0,0 +1,3 @@
+const airline = 'TAP Air Portugal'
+const plane = 'A320'
+console.log(airline.slice(-1))
---
+++ js/lib/gamebet.js
@@ -0,0 +1,54 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '3:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski'],
+  date: 'Nov 9th, 2037',
+  // odds 赔率
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const gameEvents = new Map([
+[17,'GOAL'],
+[36,'Substitution'],
+[47, 'GOAL'],
+[61,'Substitution'],
+[64, 'Yellow card'],
+[69,'Red card' ],
+[70,'Substitution'],
+[72,'Substitution'],
+[76,'GOAL'],
+[80,'GOAL'],
+[92,'Yellow card']]);
